e76ec10679a6293d25995038fd2d5b6617286dd6
Zitric <zitricwebdeveloper@gmail.com>
2019-06-15 16:05:22 +0100

    Update CommentBox.test.js

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -18,10 +18,21 @@ it("has a text area and a button", () => {
   expect(component.find("button").length).toEqual(1);
 });
 
-it("has a text area that users can type in", () => {
-  component.find("textarea").simulate("change", {
-    target: { value: "new comment" }
+describe("the text area", () => {
+  beforeEach(() => {
+    component.find("textarea").simulate("change", {
+      target: { value: "new comment" }
+    });
+    component.update();
   });
-  component.update();
-  expect(component.find("textarea").prop("value")).toEqual("new comment");
+
+  it("has a text area that users can type in", () => {
+    expect(component.find("textarea").prop("value")).toEqual("new comment");
+  });
+
+  it("when form is submitted, text area gets emtied", () => {
+    component.find("form").simulate("submit");
+    component.update();
+    expect(component.find("textarea").prop("value")).toEqual("");
+  });
 });
