6c0f7196f68ebeb5147077c34c49d658027fc5cb
Zitric <zitricwebdeveloper@gmail.com>
2019-07-09 20:03:24 +0100

    formated applied properly

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Router } from "react-router-dom";
-import { createBrowserHistory } from "history";
-import Root from "Root";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Router } from 'react-router-dom'
+import { createBrowserHistory } from 'history'
+import Root from 'Root'
 
-import App from "components/App/App";
+import App from 'components/App/App'
 
-import "index.scss";
+import 'index.scss'
 
-import * as serviceWorker from "./serviceWorker";
+import * as serviceWorker from './serviceWorker'
 
-const history = createBrowserHistory();
+const history = createBrowserHistory()
 
 ReactDOM.render(
-    <Root>
-        <Router history={history}>
-            <App />
-        </Router>
-    </Root>,
-    document.querySelector("#root")
-);
+  <Root>
+    <Router history={history}>
+      <App />
+    </Router>
+  </Root>,
+  document.querySelector('#root'),
+)
 
-serviceWorker.unregister();
+serviceWorker.unregister()
